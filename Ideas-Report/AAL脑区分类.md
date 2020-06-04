01e83068e1c4b98ebf7110dad3661ab35e4a4a73
RubiscoP <316713104@qq.com>
2020-06-04 12:50:09 +0800

    change

---
+++ Ideas-Report/AAL脑区分类.md
@@ -0,0 +1,17 @@
+# AAL脑区分类
+
+* https://www.cnblogs.com/minks/p/5480818.html（AAL分区中文名称）
+* 英文名称表格一张，见```AAL_LIST.pdf```只是名称表格，速查用。
+* 阿兹海默症文章一篇```Iwann13_oasis_aal_draft.pdf```
+
+* https://wenku.baidu.com/view/64d84be36bdc5022aaea998fcc22bcd126ff4224.html（功能简介）
+
+* https://zh.wikipedia.org/wiki/布罗德曼分区系统（不是我们的）
+
+# 烟雾病和相关脑区
+
+* https://en.wikipedia.org/wiki/Moyamoya_disease （维基简介）
+* 
+
+
+
--- code/sampleEntropy/samptest.m
+++ code/sampleEntropy/samptest.m
@@ -1,14 +1,29 @@
-x = linspace(1,100);
-y_sin = sin(x);
-r_sin = 0.2*std(y_sin);
+clc;
+clear all;
 
-y_rand = rand(1,100);
-r_rand = 0.2*std(y_rand);
+x = linspace(0,300);
+y_sin = sin(x);
+r_sin = std(y_sin);
 
 m = 5;
+tau = 1;
 
-sampe_sin = sampen(y_sin,m,r_sin,'chebychev');
-disp(sampe_sin);
-% disp(r_sin);
-sampe_rand = sampen(y_rand,m,r_rand,'chebychev');
-disp(sampe_rand);
+% [e_sin] = multiscaleSampleEntropy(y_sin,2,r_sin,1);
+% disp(e_sin);
+
+y_rand = rand(1,300);
+r_rand = std(y_rand);
+
+% [e_rand] = multiscaleSampleEntropy(y_rand,m,r_rand,tau);
+% disp(e_rand);
+
+e_pre_sin = sampen(y_sin,m,r_sin);
+disp(e_pre_sin);
+
+e_pre_rand = sampen(y_rand,m,r_rand);
+disp(e_pre_rand);
+
+
+
+
+
